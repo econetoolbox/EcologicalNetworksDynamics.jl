01ab5737513a241a79342190e8c0e4ebb2bd4fd5
Iago Bonnici <iago.bonnici@umontpellier.fr>
2025-07-24 17:02:06 +0200

    🌱 Basic NodesView.

--- src/Networks/primitives.jl
+++ src/Networks/primitives.jl
@@ -1,4 +1,18 @@
 """
+Introduce a new class of nodes.
+"""
+function add_class!(n::Network, parent::Symbol, name::Symbol, r::Restriction)
+    (; classes) = n
+    name in keys(classes) && argerr("There is already a class named :$name.")
+    parent = classes[parent]
+    classes[name] = Class(name, parent, r)
+end
+add_class!(n::Network, p::Symbol, c::Symbol, r::Range) = add_class!(n, p, c, Range(r))
+add_class!(n::Network, p::Symbol, c::Symbol, mask) =
+    add_class!(n, p, c, sparse_from_mask(mask))
+export add_class!
+
+"""
 Add new data to every node in the class.
 The given vector will be moved into a protected Entry/Field:
 don't keep reference around or leak them to end users.
@@ -18,3 +32,20 @@ function add_field!(c::Class, fname::Symbol, v::Vector{T}) where {T}
     data[name] = Entry{V}(Field{V}(v))
 end
 export add_field!
+
+"""
+Get a node-level view into network data.
+"""
+function nodes_view(n::Network, class::Symbol, data::Symbol)
+    (; classes) = n
+    class in keys(classes) || argerr("There is no class :$class in the network.")
+    c = classes[class]
+
+    data in keys(c.data) || argerr("There is no data :$data in class :$class.")
+    entry = c.data[data]
+
+    R = restrict_type(c)
+    T = eltype(data)
+    View{R,T}(entry, c)
+end
+export view
--- src/Networks/web.jl
+++ src/Networks/web.jl
@@ -1,2 +1,12 @@
 # HERE: implement.
 struct Web end
+
+"""
+Fork web, called when COW-pying the whole network.
+"""
+function fork(w::Web)
+    (;) = w
+    Web()
+end
+
+entries(::Web) = ()
