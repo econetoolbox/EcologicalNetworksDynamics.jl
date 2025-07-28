6ca286ec14cda1e943907f5da1ce7da488c12501
Iago Bonnici <iago.bonnici@umontpellier.fr>
2025-07-28 14:59:41 +0200

    ✨ Export nodes data under various forms.

---
+++ src/Networks/nodes_exports.jl
@@ -0,0 +1,66 @@
+# Extract regular data from a nodes view.
+
+"""
+Obtain a copy of the underlying vector.
+The obtained size and order correspond to the associated nodes class.
+"""
+to_vec(v::NodesView) = read(collect, v)
+export to_vec
+
+"""
+Obtain a sparse vector
+whose size and order correspond to the parent nodes class,
+but with only the focal class values set.
+Optionally require grand-parent class instead etc.
+"""
+function to_sparse(v::NodesView)
+    T = eltype(v)
+    class = Networks.class(v)
+    isnothing(class.parent) && err("The root nodes class is not sparse within another.")
+    res = spzeros(T, n_nodes(class.parent))
+    read(entry(v), class.restriction) do (v, r)
+        for (i_local, i_parent) in enumerate(indices(r))
+            res[i_parent] = v[i_local]
+        end
+    end
+    res
+end
+function to_sparse(v::NodesView, parent_name::Symbol)
+    T = eltype(v)
+    class = Networks.class(v)
+    isnothing(class.parent) && err("The root nodes class is not sparse within another.")
+    # Find adequate parent class.
+    parent = class.parent
+    while true
+        parent.name == parent_name && break
+        parent = parent.parent
+        isnothing(parent) &&
+            err("Node class :$parent_name does not superclass $(class.name).")
+    end
+    # Use it to fill the result.
+    res = spzeros(T, n_nodes(parent))
+    read(entry(v), class.index, parent.index) do (v, local_index, parent_index)
+        for (label, i_local) in local_index
+            i_parent = parent_index[label]
+            res[i_parent] = v[i_local]
+        end
+    end
+    res
+end
+export to_sparse
+
+"""
+Obtain data under the form of a mapping from nodes class labels.
+"""
+function to_map(v::NodesView)
+    T = eltype(v)
+    class = Networks.class(v)
+    res = OrderedDict{Symbol,T}()
+    read(entry(v), class.index) do (v, index)
+        for (label, i) in index
+            res[label] = v[i]
+        end
+    end
+    res
+end
+export to_map
--- test/networks/nodes.jl
+++ test/networks/nodes.jl
@@ -1,6 +1,8 @@
 module TestNodes
 
 using Test
+using SparseArrays
+using OrderedCollections
 using Main.TestUtils
 using EcologicalNetworksDynamics.Networks
 
@@ -115,26 +117,20 @@ end
 
     n = Network()
     add_class!(n, :species, "abcde")
-    add_field!(n, :species, :biomass, zeros(5))
     add_field!(n, :species, :mortality, collect(1:5) ./ 10)
     add_subclass!(n, :species, :producers, Bool[0, 1, 1, 0, 1])
     add_field!(n, :producers, :growth, [0.15, 0.25, 0.35])
-    add_class!(n, :nutrients, "uvw")
-    add_field!(n, :nutrients, :turnover, [4, 5, 6])
     add_subclass!(n, :producers, :mineral_bound, Bool[1, 0, 1])
     add_field!(n, :mineral_bound, :consumption_rate, [10, 50])
     @test is_disp(n, strip("""
-             Network with 8 nodes and 5 fields:
+             Network with 5 nodes and 3 fields:
                Nodes:
                  mineral_bound (2): [:b, :e]
                    consumption_rate: [10, 50]
-                 nutrients (3): [:u, :v, :w]
-                   turnover: [4, 5, 6]
                  producers (3): [:b, :c, :e]
                    growth: [0.15, 0.25, 0.35]
                  root: <no data>
                  species (5): [:a, :b, :c, :d, :e]
-                   biomass: [0.0, 0.0, 0.0, 0.0, 0.0]
                    mortality: [0.1, 0.2, 0.3, 0.4, 0.5]
              """))
     # Fork to check later COW.
@@ -170,33 +166,85 @@ end
 
     # Check COW aliasing/mutation.
     @test is_disp(n, strip("""
-             Network with 8 nodes and 5 fields:
+             Network with 5 nodes and 3 fields:
                Nodes:
                  mineral_bound (2): [:b, :e]
                    consumption_rate: [2, 20]
-                 nutrients (3): [:u, :v, :w]
-                   turnover'2: [4, 5, 6]
                  producers (3): [:b, :c, :e]
                    growth'2: [0.15, 0.25, 0.35]
                  root: <no data>
                  species (5): [:a, :b, :c, :d, :e]
-                   biomass'2: [0.0, 0.0, 0.0, 0.0, 0.0]
                    mortality: [1.0, 1.0, 0.3, 0.4, -0.5]
              """))
     @test is_disp(f, strip("""
-             Network with 8 nodes and 5 fields:
+             Network with 5 nodes and 3 fields:
                Nodes:
                  mineral_bound (2): [:b, :e]
                    consumption_rate: [10, 50]
-                 nutrients (3): [:u, :v, :w]
-                   turnover'2: [4, 5, 6]
                  producers (3): [:b, :c, :e]
                    growth'2: [0.15, 0.25, 0.35]
                  root: <no data>
                  species (5): [:a, :b, :c, :d, :e]
-                   biomass'2: [0.0, 0.0, 0.0, 0.0, 0.0]
                    mortality: [0.1, 0.2, 0.3, 0.4, 0.5]
              """))
+
+end
+
+@testset "Nodes exports." begin
+
+    n = Network()
+    add_class!(n, :species, "abcde")
+    add_field!(n, :species, :mortality, collect(1:5) ./ 10)
+    add_subclass!(n, :species, :producers, Bool[0, 1, 1, 0, 1])
+    add_field!(n, :producers, :growth, [0.15, 0.25, 0.35])
+    add_subclass!(n, :producers, :mineral_bound, Bool[1, 0, 1])
+    add_field!(n, :mineral_bound, :consumption_rate, [10, 50])
+    @test is_disp(n, strip("""
+             Network with 5 nodes and 3 fields:
+               Nodes:
+                 mineral_bound (2): [:b, :e]
+                   consumption_rate: [10, 50]
+                 producers (3): [:b, :c, :e]
+                   growth: [0.15, 0.25, 0.35]
+                 root: <no data>
+                 species (5): [:a, :b, :c, :d, :e]
+                   mortality: [0.1, 0.2, 0.3, 0.4, 0.5]
+             """))
+
+    # Exports within root parent class.
+    g = nodes_view(n, :producers, :growth)
+
+    v = to_vec(g)
+    @test typeof(v) === Vector{Float64}
+    @test v == [0.15, 0.25, 0.35]
+
+    s = to_sparse(g)
+    @test typeof(s) === SparseVector{Float64,Int}
+    @test s == sparse([0, 0.15, 0.25, 0, 0.35])
+
+    m = to_map(g)
+    @test typeof(m) === OrderedDict{Symbol,Float64}
+    @test m == OrderedDict(:b => 0.15, :c => 0.25, :e => 0.35)
+
+    # Exports within non-root parent class.
+    r = nodes_view(n, :mineral_bound, :consumption_rate)
+
+    v = to_vec(r)
+    @test typeof(v) === Vector{Int}
+    @test v == [10, 50]
+
+    s = to_sparse(r)
+    @test typeof(s) === SparseVector{Int,Int}
+    @test s == sparse([10, 0, 50])
+
+    m = to_map(r)
+    @test typeof(m) === OrderedDict{Symbol,Int}
+    @test m == OrderedDict(:b => 10, :e => 50)
+
+    # Export several parents at once.
+    s = to_sparse(r, :root)
+    @test typeof(s) === SparseVector{Int,Int}
+    @test s == sparse([0, 10, 0, 0, 50])
 
 end
 
