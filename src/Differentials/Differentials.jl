c483a2ee9de98edbc8d4251b5b6f15e373a40a1c
Iago Bonnici <iago.bonnici@umontpellier.fr>
2025-11-05 15:12:59 +0100

    🔧 Organize generated artifacts ⚠ world age error.

--- src/Differentials/Differentials.jl
+++ src/Differentials/Differentials.jl
@@ -45,6 +45,9 @@ using ..Networks
 using Crayons
 using MacroTools
 
+const D = Differentials
+const I = Iterators
+
 include("./errors.jl")
 
 # Naming conventions within generated code.
@@ -57,8 +60,20 @@ include("./errors.jl")
 
 include("./growth.jl")
 
+
 """
-Assemble all pieces of code into a `dudt` function.
+Elementary differential equation to feed the solver with.
+Feeds from all variables in the model,
+and from a data type that has been generated along
+with the associated method code.
+"""
+function dudt! end
+
+"""
+Assemble all pieces of code
+into a generated type and its associated `dudt!` method.
+Return a working instance of this type,
+corresponding to values in the network.
 """
 function generate_dudt(n::Network)
 
@@ -82,16 +97,20 @@ function generate_dudt(n::Network)
     append!(data)
 
     # Sort data by name for arguments consistency in generated struct constructor.
-    all_data = sort!(collect(all_data), by=first)
+    all_data = sort!(collect(all_data); by = first)
     fields = (:($k::$(typeof(v))) for (k, v) in all_data)
 
-    code = quote
-
-        mutable struct Data
+    # Generate mutable type to hold the required data and keep it up-to date.
+    type = quote
+        # This name ---vvvv changed to something hygienic if evaluated.
+        mutable struct DATA
             $(fields...)
         end
+    end
 
-        function dudt!(dU, U, data::Data, time)
+    # Generate simulation method.
+    diff! = quote
+        function $D.dudt!(dU, U, data::DATA, time)
 
             # Destructure input data into variables used within generated pieces.
             (; $(map(first, all_data)...)) = data
@@ -99,15 +118,62 @@ function generate_dudt(n::Network)
             $(growth.args...)
 
         end
-
-        instance = Data($(map(last, all_data)...))
-
-        (dudt!, Data, instance)
     end
 
-    code
+    # The above two pieces of generated code
+    # constitute the simulation program for a whole class of models,
+    # possibly containing more/less edges/nodes than the one received
+    # and/or different data values.
+    # Avoid generating/compiling too many julia `structs` and `dudt!` methods,
+    # by caching these artifacts.
+    # Only models with more/less compartments, variables
+    # and/or different functional responses will invalidate this program.
+    key = (type, diff!)
+    typename = if haskey(GEN_TYPES, key)
+        # Skip evaluation if already done.
+        GEN_TYPES[key]
+    else
+        # Otherwise evaluation is needed.
+        # Replace type name with a hygienic one
+        # and create the struct + associated method.
+        name = gensym(:Data)
+        setindexpr!(type, name, 2, 2)
+        setindexpr!(diff!, name, 2, 1, 4, 2)
+        eval(quote
+            $type
+            $diff!
+        end)
+        GEN_TYPES[key] = name
+    end
+
+    # Retrieve the generated type.
+    Data = getfield(D, typename)
+
+    # Construct and return instance of it.
+    Data(map(last, all_data)...)
 
 end
 export generate_dudt
+
+# Module-level collection of generated type names.
+const GEN_TYPES = Dict{Tuple{Expr,Expr},Symbol}()
+
+#-------------------------------------------------------------------------------------------
+
+"""
+Convenience index into julia expressions
+to avoid awkward `.args[i].args[j].args[k].args[l]`..
+"""
+function getindexpr(x::Expr, i::Int...)
+    for i in i
+        x = x.args[i]
+    end
+    x
+end, function setindexpr!(x::Expr, v, i::Int...)
+    for i in I.take(i, length(i) - 1)
+        x = x.args[i]
+    end
+    x.args[last(i)] = v
+end
 
 end
--- test/differentials/growth.jl
+++ test/differentials/growth.jl
@@ -8,21 +8,18 @@ add_class!(n, :species, "abcdefg")
 
 add_subclass!(n, :species, :producers, Bool[1, 0, 1, 1, 0, 1, 0])
 add_field!(n, :producers, :r, [1.1, 1.2, 1.3, 1.4])
-add_field!(n, :producers, :K, [10, 20, 30, 40])
+add_field!(n, :producers, :K, [10.0, 20.0, 30.0, 40.0])
 
 alpha = [
     0 1 6 0
     2 8 3 0
     0 9 4 7
-    5 0 0 0
+    5 0 0 0.0
 ]
 top = FullReflexive(alpha)
 add_web!(n, :competition, (:producers, :producers), top)
 add_field!(n, :competition, :intensity, edges_vec(top, alpha))
 
-code = generate_dudt(n)
-(f!, Data, instance) = eval(code)
-# HERE: `Data` is defined at toplevel scope.
-# How to have it hygienic/reusable for other instances?
+d = generate_dudt(n)
 
 end
