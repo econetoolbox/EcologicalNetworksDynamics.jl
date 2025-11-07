b8cbbe8e7c8c5251395b84726509b94647a3d8a1
Iago Bonnici <iago.bonnici@umontpellier.fr>
2025-11-07 12:11:39 +0100

    🗲 Vectorize growth inner loop.

--- src/Differentials/growth.jl
+++ src/Differentials/growth.jl
@@ -6,7 +6,7 @@ The general idea revolves around, for every such species `i`:
     dB[i] += r[i] * G[i] * B[i]
 
 With `r` a user-defined growth rate,
-and `G[i]` calculated by iterating over every producer competitor:
+and `G[i]` calculated by iterating over every competitor producer:
 
     G[i] = 1 - 1/K[i] * sum_j(α_ij * B[j])
 
@@ -30,6 +30,11 @@ function generate_growth(n::Network)
     r = read(collect, prods.data[:r])
     K = read(collect, prods.data[:K])
 
+    n_producers = length(prods)
+
+    # Scratch space to collect compact producers biomass.
+    U_producers = zeros(n_producers)
+
     if top isa FullReflexive
 
         # Retrieve full flat edge data.
@@ -37,16 +42,18 @@ function generate_growth(n::Network)
 
         # And here is how to use it.
         code = quote
-            # TODO: avoid zipping and iterate 2 by 2 in [(r, K), (r, K), ...], any better?
-            for (i_producer, (i_root, r, K)) in enumerate(zip(i_producers, r, K))
-                sum = 0 # (over competitors)
-                for (j_producer, j_root) in enumerate(i_producers)
-                    i_alpha = (i_producer - 1) * n_producers + j_producer # TODO: `+=1` ?
-                    alpha_ij = alpha[i_alpha]
-                    sum += alpha_ij * U[j_root]
+            for (i_prod, i_root) in enumerate(i_producers)
+                U_producers[i_prod] = U[i_root]
+            end
+            ij = 0
+            for (i, (i_root, r, K)) in enumerate(zip(i_producers, r, K))
+                sum = 0.0 # (over competitors)
+                @simd for j in eachindex(U_producers)
+                    ij += 1
+                    @inbounds sum += alpha[ij] * U_producers[j]
                 end
-                G = 1 - 1 / K * sum
-                dU[i_root] = r * G * U[i_root] # First assignment.
+                G = 1.0 - 1.0 / K * sum
+                dU[i_root] = r * G * U_producers[i] # First assignment.
             end
         end
 
@@ -54,7 +61,7 @@ function generate_growth(n::Network)
         throw("Unimplemented producers competition topology flavour: $(typeof(top)).")
     end
 
-    data = (; r, K, alpha, i_producers, n_producers = length(prods))
+    data = (; r, K, alpha, i_producers, U_producers)
 
     (code, data)
 
