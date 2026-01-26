467793ecc147d4713acf3ce28fea23a657e0b717
Iago Bonnici <iago.bonnici@umontpellier.fr>
2026-01-26 13:06:10 +0100

    🧹 Extract typical "Reflexive Web" component.

--- src/components/web.jl
+++ src/components/web.jl
@@ -1,4 +1,137 @@
 """
+Typical setup for a component bringing a new reflexive web to the network.
+"""
+macro reflexive_web_component(input...)
+    quote
+        $define_reflexive_web_component($__module__, $(Meta.quot.(input)...))
+        nothing
+    end
+end
+
+# If extension points are used, this form is more ergonomic,
+# and the calling module is inferred from extension points.
+function define_reflexive_web_component(
+    mod::Option{Module},
+    # Web name (capitalized and not).
+    web::Symbol,
+    Web::Symbol,
+    # Dependent/Brought class that the web is reflexive within.
+    class::Symbol,
+    Class::Union{Symbol,Expr}; # Path to the actual class component.
+    # Property name, if different from web name.
+    prop = (web, Web),
+    # Extension points.
+    expand_from_matrix! = (raw, topology, A) -> nothing,
+)
+    prop, Prop = prop
+    Web_ = Symbol(Web, :_) # Blueprints module name.
+    _Web = Symbol(:_, Web) # Component type name.
+    w, c, p = Meta.quot.((web, class, prop)) # Symbol names.
+    if isnothing(mod)
+        mod = parentmodule(expand_from_matrix!)
+    end
+    xp = quote
+
+        # ==================================================================================
+        # Blueprints for the component.
+        module $Web_
+        import EcologicalNetworksDynamics:
+            Blueprint, Framework, Networks, GraphDataInputs, @blueprint, @get, @ref
+        using .GraphDataInputs
+        using .Framework
+        using .Networks
+        const F = Framework
+        const Class = $mod.$Class
+        const _Class = typeof(Class)
+
+        #-----------------------------------------------------------------------------------
+        # From matrix.
+
+        mutable struct Matrix <: Blueprint
+            A::@GraphData SparseMatrix{:bin}
+            $class::Brought(Class)
+            Matrix(A, $class = Class) = new(@tographdata(A, SparseMatrix{:bin}), $class)
+        end
+
+        # Infer number of class nodes from matrix size.
+        F.implied_blueprint_for(bp::Matrix, ::_Class) = Class(size(bp.A, 1))
+        @blueprint Matrix "boolean matrix of $($w) links"
+        export Matrix
+
+        function F.early_check(bp::Matrix)
+            (; A) = bp
+            n, m = size(A)
+            n == m ||
+                checkfails("The adjacency matrix of size $((m, n)) is not squared.")
+        end
+
+        function F.late_check(raw, bp::Matrix)
+            (; A) = bp
+            n = @get raw.$class.number
+            @check_size A (n, n)
+        end
+
+        F.expand!(raw, bp::Matrix) = expand_from_matrix!(raw, bp.A)
+        function expand_from_matrix!(raw, A)
+            topology = SparseReflexive(A)
+            add_web!(raw, $p, ($c, $c), topology)
+            $expand_from_matrix!(raw, topology, A)
+        end
+
+        #-----------------------------------------------------------------------------------
+        # From ajacency list.
+
+        mutable struct Adjacency <: Blueprint
+            A::@GraphData {Adjacency}{:bin} # (refs are either numbers or names)
+            $class::Brought(Class)
+            Adjacency(A, $class = Class) =
+                new(@tographdata(A, {Adjacency}{:bin}), $class)
+        end
+
+        # Infer number or names of class nodes from the lists.
+        F.implied_blueprint_for(bp::Adjacency, ::_Class) = Class(refspace(bp.A))
+
+        @blueprint Adjacency "adjacency list of $($w) links"
+        export Adjacency
+
+        function F.late_check(raw, bp::Adjacency)
+            (; A) = bp
+            index = @ref raw.$class.index
+            @check_list_refs A $c index
+        end
+
+        function F.expand!(raw, bp::Adjacency)
+            index = @ref raw.$class.index
+            A = to_sparse_matrix(bp.A, index, index)
+            expand_from_matrix!(raw, A)
+        end
+
+        end
+
+        # ==================================================================================
+        # Component and generic constructors.
+
+        @component $Web{Internal} requires($Class) blueprints($Web_)
+
+        # Precise edges specifications.
+        function (::$_Web)(A)
+            A = @tographdata A {SparseMatrix, Adjacency}{:bin}
+            if A isa AbstractMatrix
+                $Web.Matrix(A)
+            else
+                $Web.Adjacency(A)
+            end
+        end
+
+    end
+    mod.eval.(xp.args)
+    define_web_properties(mod, prop, Prop, :(depends($Web)))
+end
+
+
+# ==========================================================================================
+
+"""
 Some webs are not directly defined by a component,
 e.g. 'producers links' is defined by the foodweb,
 but the need the same exposure: use it to expose them.
@@ -16,7 +149,7 @@ function define_web_properties(
     Web::Symbol,
     deps::Expr, # As in a regular call to @method.
 )
-    s = Meta.quot(web)
+    w = Meta.quot(web)
     M = Symbol(Web, :Methods) # Create submodule to not pollute invocation scope..
     m = :(mod($mod)) # .. but still evaluate dependencies within the invocation module.
     xp = quote
@@ -29,10 +162,10 @@ function define_web_properties(
         using .Framework
         using .Views
 
-        web(m::Internal) = Networks.web(m, $s)
+        web(m::Internal) = Networks.web(m, $w)
         topology(m::Internal) = web(m).topology
         number(m::Internal) = m |> topology |> n_edges
-        mask(::Internal, m::Model) = edges_mask_view(m, $s)
+        mask(::Internal, m::Model) = edges_mask_view(m, $w)
         @method $m $M.topology $deps read_as($web._topology)
         @method $m $M.mask $deps read_as($web.matrix, $web.mask)
         @method $m $M.number $deps read_as($web.n_links, $web.n_edges)
